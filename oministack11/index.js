773af35a52d79c9d103ffeebdb74dbf9d9c48663
Guilherme B.(Emaloka) <guih.smi2@outlook.com>
2020-03-25 03:02:02 +0000

    Att Oministack11

--- oministack11/index.js
+++ oministack11/index.js
@@ -2,7 +2,13 @@ const express  = require('express');
 const app = express();
 const PORT = 3333;
 
+app.get('/', (request,response) => {
+   response.json({
+      evento: "Evento Oministack",
+      aluno: "Jamaluco"
+   });
+});
 
 app.listen(PORT, () => {
-     console.log("Servidor Rodando na porta: " + PORST)
+     console.log("Servidor Rodando na porta: " + PORT);
 });
