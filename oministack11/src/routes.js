635b8a30d2e70ccf8a21e57fa59008a53d2a3dae
Guilherme B.(Emaloka) <guih.smi2@outlook.com>
2020-03-28 17:50:20 +0000

    Semana Att

--- oministack11/src/routes.js
+++ oministack11/src/routes.js
@@ -1,6 +1,15 @@
 const express  = require('express');
 const router  = express.Router();
 
+
+// POST : ONGS
+router.post('/ongs', (request , response) => {
+    const { name, email , whatspp, city , uf} = request.body;
+    response.json(request.body);
+});
+
+
+// Area de Teste - Para Imsomnia
 router.post('/users', (request,response) => {
     const body = request.body;
     console.log(body);
@@ -14,6 +23,11 @@ router.get('/users/:id', (request ,response) => {
    const params = request.params; 
    console.log(params);
    response.send('Foi doido');
+});
+
+
+router.get('/users', (request , response) => {
+    response.send('Hello Users');
 });
 
 module.exports = router;
